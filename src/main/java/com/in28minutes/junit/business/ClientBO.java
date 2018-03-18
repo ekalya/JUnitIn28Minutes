e7e0b0f921299b7696b87219978b2e04c20505ca
Elisha Kalya <elisha.kalya@gmail.com>
2018-03-17 22:08:05 -0600

    Re-introduced spaces on ClientBO.java class

--- src/main/java/com/in28minutes/junit/business/ClientBO.java
+++ src/main/java/com/in28minutes/junit/business/ClientBO.java
@@ -1,6 +1,7 @@
 package com.in28minutes.junit.business;
 
 import java.util.List;
+
 import com.in28minutes.junit.business.exception.DifferentCurrenciesException;
 import com.in28minutes.junit.model.Amount;
 import com.in28minutes.junit.model.Product;
