4f12ccc1f0bdbe61d880931b69bc60f02acca5df
Elisha Kalya <elisha.kalya@gmail.com>
2018-03-17 22:04:28 -0600

    Closed spaces on ClientBO.java class

--- src/main/java/com/in28minutes/junit/business/ClientBO.java
+++ src/main/java/com/in28minutes/junit/business/ClientBO.java
@@ -1,7 +1,6 @@
 package com.in28minutes.junit.business;
 
 import java.util.List;
-
 import com.in28minutes.junit.business.exception.DifferentCurrenciesException;
 import com.in28minutes.junit.model.Amount;
 import com.in28minutes.junit.model.Product;
